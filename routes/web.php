94e58e09366cc1f39282ea95119b2dbe6bf8a8aa
Adbul Aziz <aziz97cse@gmail.com>
2020-01-06 23:32:59 +0600

    Login route change done

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/admin', function () {
+Route::get('/dashboard', function () {
     return view('backend.dashboard.index');
 });
 
