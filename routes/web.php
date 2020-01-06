45c657ae58cf9c0622678327265be946de1fe7a5
Adbul Aziz <aziz97cse@gmail.com>
2020-01-06 22:37:53 +0600

    Registration And Login Complete

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('backend.dashboard.index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
