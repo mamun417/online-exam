7322d8d4e7485e1bee42f32557c24a25ac711507
Adbul Aziz <aziz97cse@gmail.com>
2020-01-06 21:16:08 +0600

    dashboard templating file add to git

---
+++ resources/views/backend/element/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    <div class="pull-right">
+        <strong>----</strong>
+    </div>
+    <div>
+        <strong>Copyright</strong> Fictionsoft &copy; 2020
+    </div>
+</div>
