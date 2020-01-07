fa34674bd12e96120719e039226402a89a85f373
Adbul Aziz <aziz97cse@gmail.com>
2020-01-07 12:12:40 +0600

    department view folder, controller, model, create and table migration complete

---
+++ database/migrations/2020_01_07_055358_create_departments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->tinyInteger('is_deleted')->default(0)->nullable();
+            $table->tinyInteger('is_active')->default(1)->nullable();
+            $table->string('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+}
